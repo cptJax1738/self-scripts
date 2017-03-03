10f7aaa9d1612eb79f14ef911f2bcdeda710a226
Yoh <cptjax@protonmail.ch>
2017-03-03 00:05:32 -0500

    first commit

---
+++ python-setup-script.sh
@@ -0,0 +1,5 @@
+cd Python;mkdir py-structure; cd py-structure; mkdir docs; mkdir sample; mkdir tests;> LICENSE && > README.rst && > requirements.txt && > setup.py 
+cd docs; > conf.py && > index.rst
+cd; cd Python;cd py-structure
+cd sample;> core.py && > helpers.py && > __init__.py
+cd ;cd Python; cd py-structure; cd tests; > test_advanced.py && > test_basic.py
